3f79944888f342bafa8679459116111596ff0224
HamletMYan <hammkrtchyan7@gmail.com>
2024-08-15 20:43:35 +0400

    first commit

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>
+            {{ foodName }}
+            <img v-show="foodIsFavorite" src="https://icons.iconarchive.com/icons/hopstarter/soft-scraps/256/Button-Favorite-icon.png">
+        </h2>
+        <p>{{ foodDesc }}</p>
+        <button @:click="toggleFavorite">Favorite</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['foodName', 'foodDesc', 'isFavorite'],
+    data() {
+        return {
+            foodIsFavorite: this.isFavorite
+        }
+    },
+    methods:{
+        toggleFavorite(){
+            this.foodIsFavorite = !this.foodIsFavorite
+        }
+    }
+}
+</script>
+
+<style>
+    img {
+        height: 1.5em;
+        float: right;
+    }
+</style>
